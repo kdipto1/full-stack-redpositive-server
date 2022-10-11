b4bc4199fa6c0e44359066a0f403bfd47a493341
kdipto1 <kdipto@outlook.com>
2022-10-11 22:27:14 +0600

    server setup completed

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+//Test purpose api
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Listening from port ${port}`);
+});
