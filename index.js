3deffc6d69937c0de7306e2aeae0406d211636f4
kdipto1 <kdipto@outlook.com>
2022-10-12 19:35:04 +0600

    mongodb connected succesfully

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
-const express = require("express")
-const app = express()
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
@@ -8,11 +9,23 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
-
+// MongDB connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6d8lyhe.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const dataCollection = client.db("data").collection("candidates");
+    console.log(uri);
+  } finally {
+    
+  }
+}
+run().catch(console.dir)
 
 
 
