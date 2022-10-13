d9f75bdd713b5971d7ccf60251c7089c237013bb
kdipto1 <kdipto@outlook.com>
2022-10-13 19:59:56 +0600

    update candidate api created

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
-require("dotenv").config();
-const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 // MongDB connect
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6d8lyhe.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qjyt1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+// console.log(uri);
 async function run() {
   try {
     await client.connect();
-    const dataCollection = client.db("data").collection("candidates");
-    console.log(uri);
+    const dataCollection = client.db("candidate").collection("candidates");
+    // console.log(client);
+    //Add candidates
+    app.post("/addCandidate", async (req, res) => {
+      const newCandidate = req.body;
+      const result = await dataCollection.insertOne(newCandidate);
+      res.send(result);
+    });
+    app.get("/candidates", async (req, res) => {
+      const query = {};
+      const candidates = await dataCollection.find(query).toArray();
+      res.send(candidates);
+    });
+    //update candidate api
+    app.put("/updateCandidates/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = { $set: data };
+      const option = { upsert: true };
+      const result = await dataCollection.updateOne(filter, updateDoc, option);
+      res.send(result);
+    });
   } finally {
-    
   }
 }
-run().catch(console.dir)
-
-
+run().catch(console.dir);
 
 //Test purpose api
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World - Crud!");
 });
 app.listen(port, () => {
   console.log(`Listening from port ${port}`);
